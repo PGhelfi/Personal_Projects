4ed9c45c628fb0acbf1726d719641b6be90cc240
PGhelfi <pedroghelfi@gmail.com>
2026-09-11 14:57:46 -0300

    Update variable names from PriceRatio to PriceVariation

--- Basic_Projects/Procedural/Discount_Calc/Pedro Ghelfi Raza_Ag6_DS_I.py
+++ Basic_Projects/Procedural/Discount_Calc/Pedro Ghelfi Raza_Ag6_DS_I.py
@@ -1,6 +1,6 @@
-# Simple procedural discount calculator for an item being purchased based on a price ratio.
+# Simple procedural discount calculator for an item being purchased based on a price variation.
 
-# Discount variables to be applied based on the given price ratio inputted.
+# Discount variables to be applied based on the given price variation inputted.
 DiscountLow = 0.05
 DiscountMid = 0.10
 DiscountHigh = 0.15
@@ -9,23 +9,23 @@ DiscountHigh = 0.15
 Product = input("Qual o produto que voce levara hoje?: ")
 
 try:
-    PriceRatio = float(input(f"\n Por favor, insira o preco de {Product} para ser ser calculado o desconto: "))
+    PriceVariation = float(input(f"\n Por favor, insira o preco de {Product} para ser ser calculado o desconto: "))
 
 # Discount is checked, based on the low, mid, high discount criteria.
-    if PriceRatio < 200:
+    if PriceVariation < 200:
         Discount = DiscountLow
         
-    elif PriceRatio >= 200 and PriceRatio < 300:
+    elif PriceVariation >= 200 and PriceVariation < 300:
         Discount = DiscountMid
         
-    elif PriceRatio >= 300:
+    elif PriceVariation >= 300:
         Discount = DiscountHigh
     
     # Now, the discount is calculated based on the price ratio and presented to the user:
-    DiscountValue = PriceRatio * Discount
-    FinalPrice = PriceRatio - DiscountValue
+    DiscountValue = PriceVariation * Discount
+    FinalPrice = PriceVariation - DiscountValue
 
-    print (f"\n O Preco inical do item {Product} era de: R$ {PriceRatio: .2f} | "
+    print (f"\n O Preco inical do item {Product} era de: R$ {PriceVariation: .2f} | "
     f" O desconto de {Discount * 100: .0f}% foi aplicado | " f"O preco final e de R$ {FinalPrice: .2f}" )
     
 except ValueError:
